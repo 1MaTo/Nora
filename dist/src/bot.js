09d103f9b323fdcad3c196a5931020f57d11bc63
MaTo <ily4368@gmail.com>
2020-11-02 19:20:56 +0300

    change gitignore file

--- dist/src/bot.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _auth = require("../auth.json");
-
-var _auth2 = _interopRequireDefault(_auth);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-console.log("sdsd");
