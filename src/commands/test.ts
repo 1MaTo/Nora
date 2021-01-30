30c9e2e8bc247cf53bde498a5f771f83d7c6c4fb
MaTo <ily4368@gmail.com>
2021-01-30 19:44:48 +0300

    Complete reload command

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -40,7 +40,7 @@ export default class test extends SlashCommand {
 
     log(configOne, configTwo); */
     //updateSlashCommand(undefined, reloadCommand);
-    report("Hi im here and reloaded myself! ... ffff");
+    report("Hi im here and reloaded myself! NICE");
     return;
   }
 }
