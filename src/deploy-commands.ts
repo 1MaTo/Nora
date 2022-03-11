34a1b7bb1172c6dff00d3411ea29a285bd158994
1MaTo <ily4368@gmail.com>
2022-03-11 19:09:06 +0300

    disable commands

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,17 +1,19 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 import fs from "node:fs";
 import { appId, token } from "./auth.json";
 import { guildIDs } from "./utils/globals";
 
+const disabledCommands = ["upload"];
 const commands = [];
 const commandFiles = fs
   .readdirSync(__dirname + "/commandData")
   .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-  const command = require(`./commandData/${file}`);
-  commands.push(command.toJSON());
+  const command = require(`./commandData/${file}`) as SlashCommandBuilder;
+  if (!disabledCommands.includes(command.name)) commands.push(command.toJSON());
 }
 
 const rest = new REST({ version: "9" }).setToken(token);
