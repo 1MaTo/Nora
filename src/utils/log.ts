40679d1b9d5c4923a4bb7748dd283a804b4fa050
1MaTo <ily4368@gmail.com>
2022-04-13 20:28:54 +0300

    add only production logging

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -2,6 +2,6 @@ import { production, withLogs } from "./globals";
 import { addToReports } from "./reports";
 
 export const log = (...message: any) => {
-  addToReports(message);
+  if (production) addToReports(message);
   if (!production || withLogs) console.log(...message);
 };
