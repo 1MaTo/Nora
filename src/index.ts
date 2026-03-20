c8ccd846e993296688dc210d344e804f7dd7551b
mato <ily4368@gmail.com>
2026-03-20 23:03:20 +0300

    update index

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (client) => {
+  console.log(`Ready! Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
+const t = 1;
