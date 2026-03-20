0bbb8984290d2a9b19ed072941ba685023722621
mato <ily4368@gmail.com>
2026-03-20 15:38:25 +0300

    Remove old project

--- src/api/nickname/unbindNickname.ts
+++
@@ -1,8 +0,0 @@
-import { groupsKey, redisKey } from "../../redis/kies";
-import { redis } from "../../redis/redis";
-
-export const unbindNickname = async (userID: string, guildID: string) => {
-  const key = redisKey.struct(groupsKey.bindNickname, [guildID, userID]);
-  await redis.del(key);
-  return true;
-};
--- src/api/translate/formatStringWords.ts
+++
@@ -1,8 +0,0 @@
-export const formatStringWords = (str: string) => {
-  return str
-    .replace(/skill type/gim, "type")
-    .replace(/cooling time/gim, "cd")
-    .replace(/damage/gim, "dmg")
-    .replace(/casting distance/gim, "range")
-    .replace(/seconds/gim, "s");
-};
